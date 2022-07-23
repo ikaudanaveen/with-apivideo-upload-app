986a2ee2c34c8b49d4f367bb92bae22bb0fb2472
sangajala <sriram.angajala@gmail.com>
2022-07-23 08:13:30 +0100

    env variable update

--- pages/api/uploadToken.ts
+++ pages/api/uploadToken.ts
@@ -1,9 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 import ApiVideoClient from '@api.video/nodejs-client'
 
 const getUploadToken = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const client = new ApiVideoClient({ apiKey: process.env.API_KEY })
+    const client = new ApiVideoClient({ apiKey: "su4f3iUNYzCrN4SWGIn7YKZtnvzawBlVElKxd3JJVIU" })
     const uploadToken = await client.uploadTokens.createToken()
     res.status(200).json(uploadToken)
   } catch (error) {
